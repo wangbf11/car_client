d618d8ae3d5b1e3824a53e916eed5a32c2a3c8e6
wangbf11 <584623818@qq.com>
2019-05-16 23:50:42 +0800

    支付 界面

--- src/screen/home/HomeScreen.js
+++ src/screen/home/HomeScreen.js
@@ -8,6 +8,7 @@ import {
     ScrollView,
     Text,
     TouchableOpacity,
+    TextInput
 } from 'react-native';
 import actions from "../../models/actions";
 import NavigationBar from "../../components/navigationBar";
@@ -18,53 +19,50 @@ import image from '../../assets/image'
 import { NavigationEvents } from 'react-navigation';
 import { FlatGrid, SectionGrid } from 'react-native-super-grid';
 import { screenWidth, screenHeight } from '../../util/AdapterUtil'
-
+import { PasswordModal } from 'react-native-pay-password'
 class HomeScreen extends PureComponent {
     constructor(props) {
         super(props);
+        this.state = {
+            password:""
+        };
     }
 
     componentDidMount() {
-        this.setState({
-        })
+
     }
     render() {
         return (
             <View style={{ flex: 1, backgroundColor: "#fff" }}>
                 <NavigationBar navigation={this.props.navigation} isBack={false} title={"辛巴支付"} />
+                <TouchableOpacity onPress={this.payMoney.bind(this)}
+                                  style={{ height: 36, width: 100, borderColor: "#748088", borderWidth: 1, borderRadius: 3, marginTop: 18, justifyContent: "center", alignItems: "center" }}>
+                    <Text style={{ color: "#748088", fontSize: 12 }}>支付</Text>
+                </TouchableOpacity>
+
+                <PasswordModal ref='modal' onDone={(data) => { this.setState({ password: data }) }} />
             </View>
         )
+    }
+
+    payMoney() {
+        this.refs.modal.show();
     }
 }
 
 const styles = StyleSheet.create({
-    titleStyle: {
-        color: "#202020",
-        fontSize: 30
+    textInputMsg:{
+        zIndex:99,
+        position:'absolute',
     },
-    banner_img: {
-        width: '100%',
-        height: '100%',
+    textInputView:{
+        height:85/2,
+        width:85/2,
+        borderWidth:1,
+        borderColor:'#c9c7c7',
+        justifyContent:'center',
+        alignItems:'center',
     },
-    bannerView: {
-        width: '100%',
-        height: 180,
-    },
-    gridView: {
-        marginTop: 20,
-        flex: 1,
-    },
-    itemContainer: {
-        justifyContent: 'center',
-        alignItems: "center",
-        //shadowOffset: { width: 0, height: 0 }, shadowOpacity: 0.2, shadowRadius: 10, elevation: 2,
-        borderRadius: 5,
-        height: 100,
-    },
-    itemName: {
-        fontSize: 16,
-        color: 'black',
-    }
 });
 
 export default connect(
